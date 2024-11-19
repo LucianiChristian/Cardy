e68593677e1d7ed001c9c891921f2856d3115dec
Gavin Barham <gavinbarham@yahoo.com>
2024-11-18 19:50:33 -0600

    Get file upload working with CSV Helper.

--- CardyWasm/CardModel.cs
+++ CardyWasm/CardModel.cs
@@ -1,24 +1,63 @@
-﻿namespace CardyWasm;
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CardyWasm;
 
 public class CardModel
 {
+    [Name("Deity")]
     public string? Deity { get; set; }
-    public string? CardName { get; set; }
-    public int FollowerCost { get; set; }
-    public int PrayerCost { get; set; }
+    
+    [Name("Name")]
+    public string? Name { get; set; }
+    
+    [Name("Follower Cost")]
+    public string? FollowerCost { get; set; }
+    
+    [Name("Prayer Cost")]
+    public string? PrayerCost { get; set; }
+    
+    [Name("Type")]
     public string? Type { get; set; }
+    
+    [Name("Subtype")]
     public string? Subtype { get; set; }
+    
+    [Name("Species")]
     public string? Species { get; set; }
+    
+    [Name("Archetype")]
     public string? Archetype { get; set; }
+    
+    [Name("Rarity")]
     public string? Rarity { get; set; }
+    
+    [Name("Attack")]
     public int? Attack { get; set; }
+    
+    [Name("Health")]
     public int? Health { get; set; }
+    
+    [Name("Abilities")]
     public string? Abilities { get; set; }
+    
+    [Name("Passive Effects")]
     public string? PassiveEffects { get; set; }
+    
+    [Name("Activated Effects")]
     public string? ActivatedEffects { get; set; }
+    
+    [Name("Starter Deck")]
     public string? StarterDeck { get; set; }
+    
+    [Name("Champion Exclusive")]
     public string? ChampionExclusive { get; set; }
-    public string? ImageUrl { get; set; }
+    
+    [Name("Image URL")]
+    public string? ImgUrl { get; set; }
+    
+    [Name("Author")]
     public string? Author { get; set; }
-    public int VersionNumber { get; set; }
+    
+    [Name("Version #")]
+    public int Version { get; set; }
 }
