2827cd19a6e1c13bb3aefbd91be1444c548d24b3
Gavin Barham <gavinbarham@yahoo.com>
2024-11-22 18:16:03 -0600

    Rename Card to CardModel and rename CardModel to CSVModel

---
+++ CardyWasm/CSVModel.cs
@@ -0,0 +1,63 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CardyWasm;
+
+public class CSVModel
+{
+    [Name("Deity")]
+    public string? Deity { get; set; }
+    
+    [Name("Name")]
+    public string? Name { get; set; }
+    
+    [Name("Follower Cost")]
+    public string? FollowerCost { get; set; }
+    
+    [Name("Prayer Cost")]
+    public string? PrayerCost { get; set; }
+    
+    [Name("Type")]
+    public string? Type { get; set; }
+    
+    [Name("Subtype")]
+    public string? Subtype { get; set; }
+    
+    [Name("Species")]
+    public string? Species { get; set; }
+    
+    [Name("Archetype")]
+    public string? Archetype { get; set; }
+    
+    [Name("Rarity")]
+    public string? Rarity { get; set; }
+    
+    [Name("Attack")]
+    public string? Attack { get; set; }
+    
+    [Name("Health")]
+    public string? Health { get; set; }
+    
+    [Name("Abilities")]
+    public string? Abilities { get; set; }
+    
+    [Name("Passive Effects")]
+    public string? PassiveEffects { get; set; }
+    
+    [Name("Activated Effects")]
+    public string? ActivatedEffects { get; set; }
+    
+    [Name("Starter Deck")]
+    public string? StarterDeck { get; set; }
+    
+    [Name("Champion Exclusive")]
+    public string? ChampionExclusive { get; set; }
+    
+    [Name("Image URL")]
+    public string? ImgUrl { get; set; }
+    
+    [Name("Author")]
+    public string? Author { get; set; }
+    
+    [Name("Version #")]
+    public string? Version { get; set; }
+}
--- CardyWasm/CardModel.cs
+++ CardyWasm/CardModel.cs
@@ -1,63 +1,113 @@
-﻿using CsvHelper.Configuration.Attributes;
-
 namespace CardyWasm;
 
 public class CardModel
 {
-    [Name("Deity")]
-    public string? Deity { get; set; }
+    public string? Deity { get; }
+    public string? Name { get; }
+    public string? FollowerCost { get; }
+    public string? PrayerCost { get; }
+    public string? Type { get; }
+    public string? Subtype { get; }
+    public string? Species { get; }
+    public string? Archetype { get; }
+    public string? Rarity { get; }
+    public string? Attack { get; }
+    public string? Health { get; }
+    public IList<string> Abilities { get; }
+    public IList<string> PassiveEffects { get; }
+    public IList<string> ActivatedEffects { get; }
+    public string? ImgUrl { get; }
+    public string? Author { get; }
+
+    // Primary constructor
+    public CardModel(
+        string? deity,
+        string? name,
+        string? followerCost,
+        string? prayerCost,
+        string? type,
+        string? subtype,
+        string? species,
+        string? archetype,
+        string? rarity,
+        string? attack,
+        string? health,
+        string? abilities,
+        string? passiveEffects,
+        string? activatedEffects,
+        string? imgUrl,
+        string? author)
+    {
+        Deity = deity;
+        Name = name;
+        FollowerCost = followerCost;
+        PrayerCost = prayerCost;
+        Type = type;
+        Subtype = subtype;
+        Species = species;
+        Archetype = archetype;
+        Rarity = rarity;
+        Attack = attack;
+        Health = health;
+        Abilities = abilities is not null ? abilities.Split(" | ").ToList() : new List<string>();
+        PassiveEffects = passiveEffects is not null ? passiveEffects.Split(" | ").ToList() : new List<string>();
+        ActivatedEffects = activatedEffects is not null ? activatedEffects.Split(" | ").ToList() : new List<string>();
+        ImgUrl = imgUrl;
+        Author = author;
+    }
     
-    [Name("Name")]
-    public string? Name { get; set; }
-    
-    [Name("Follower Cost")]
-    public string? FollowerCost { get; set; }
-    
-    [Name("Prayer Cost")]
-    public string? PrayerCost { get; set; }
-    
-    [Name("Type")]
-    public string? Type { get; set; }
-    
-    [Name("Subtype")]
-    public string? Subtype { get; set; }
-    
-    [Name("Species")]
-    public string? Species { get; set; }
-    
-    [Name("Archetype")]
-    public string? Archetype { get; set; }
-    
-    [Name("Rarity")]
-    public string? Rarity { get; set; }
-    
-    [Name("Attack")]
-    public string? Attack { get; set; }
-    
-    [Name("Health")]
-    public string? Health { get; set; }
-    
-    [Name("Abilities")]
-    public string? Abilities { get; set; }
-    
-    [Name("Passive Effects")]
-    public string? PassiveEffects { get; set; }
-    
-    [Name("Activated Effects")]
-    public string? ActivatedEffects { get; set; }
-    
-    [Name("Starter Deck")]
-    public string? StarterDeck { get; set; }
-    
-    [Name("Champion Exclusive")]
-    public string? ChampionExclusive { get; set; }
-    
-    [Name("Image URL")]
-    public string? ImgUrl { get; set; }
-    
-    [Name("Author")]
-    public string? Author { get; set; }
-    
-    [Name("Version #")]
-    public string? Version { get; set; }
+    public CardModel(CSVModel other)
+        : this(
+            other.Deity,
+            other.Name,
+            other.FollowerCost,
+            other.PrayerCost,
+            other.Type,
+            other.Subtype,
+            other.Species,
+            other.Archetype,
+            other.Rarity,
+            other.Attack,
+            other.Health,
+            other.Abilities,
+            other.PassiveEffects,
+            other.ActivatedEffects,
+            other.ImgUrl,
+            other.Author)
+    { }
+    public string GetBottomText()
+    {
+        var bottomText = "";
+
+        if (IsNotEmpty(Type))
+        {
+            bottomText += Type;
+        }
+
+        if (IsNotEmpty(Subtype))
+        {
+            bottomText += " " + Subtype;
+        }
+
+        if (IsNotEmpty(Species) || IsNotEmpty(Archetype))
+        {
+            bottomText += " " + "-";
+        }
+
+        if (IsNotEmpty(Species))
+        {
+            bottomText += " " + Species;
+        }
+
+        if (IsNotEmpty(Archetype))
+        {
+            bottomText += " " + Archetype;
+        }
+
+        return bottomText;
+    }
+    private bool IsNotEmpty(string? str)
+    {
+        return str is not null && str !=  "";
+    }
 }
